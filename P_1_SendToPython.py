a925ca6e62724976473aff9f1c6ea4eb5788d039
RocioRRdz <114269139+RocioRRdz@users.noreply.github.com>
2023-03-04 13:04:21 -0600

    Add files via upload

---
+++ P_1_SendToPython.py
@@ -0,0 +1,19 @@
+
+import serial as s   #pyserial
+
+arduino = None #🥹🥹
+
+#arduino = s.Serial("COM3", baudrate=9600, timeout=1)
+arduino = s.Serial("/dev/cu.usbmodem1101", baudrate=9600, timeout=1)
+
+while True:
+    cadena = arduino.readline()
+    #print(cadena)  ## imprime como.... b'419\r\n'
+    cadena = cadena.decode()
+    #print(cadena)  ## imprime como...419 con saltos de linea entre impresion
+    cadena = cadena.replace("\n","")
+    cadena = cadena.replace("\r", "")
+    print(cadena * 3 )  ##<<----
+
+
+
